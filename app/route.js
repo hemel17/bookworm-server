5219de5fa5fc6164ec49095a8681150783895607
hemel17 <redewanmahmud@gmail.com>
2025-03-27 03:12:25 +0600

    app created with db connection

---
+++ app/route.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+const router = Router();
+import mongoose from "mongoose";
+
+router.get("/", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running.",
+  });
+});
+
+router.get("/health", (_req, res) => {
+  const dbStatus =
+    mongoose.connection.readyState === 1 ? "connected" : "disconnected";
+
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    database: dbStatus,
+    timeStamp: new Date().toLocaleString(),
+  });
+});
+
+export default router;
